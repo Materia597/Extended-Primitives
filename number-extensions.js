3a1c8bfa330ebfc8fbff0867e9c13f76febc33c6
Materia597 <112108517+Materia597@users.noreply.github.com>
2023-10-13 08:29:11 -0700

    Update number-extensions.js

--- number-extensions.js
+++ number-extensions.js
@@ -5,3 +5,33 @@ Number.prototype.floor = () => {
 Number.prototype.ceil = () => {
     return Math.ceil(this)
 }
+
+Number.prototype.round = () => {
+    return Math.round(this)
+}
+
+Number.prototype.sqrt = () => {
+    return Math.sqrt(this)
+}
+
+Number.prototype.power = (power) => {
+    return this ** power
+}
+
+Number.prototype.squared = () => {
+    return this * this
+}
+
+Number.prototype.cubed = () => {
+    return this * this * this
+}
+
+Number.prototype.mod = (mod) => {
+    return this % mod
+}
+
+Number.prototype.largestPowerOf2 = () => {
+    let power = -32
+    while(this - 2 ** power > 2 ** power) power++
+    
+}
